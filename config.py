6339d71eb4098d35b46e2fcba657b8f8960d88bb
Agnar22 <agnar.bjornstad@gmail.com>
2020-10-29 14:09:57 +0100

    Add more countries to visualize.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Data related parameters.
 DATA_PATH = 'Data/owid-covid-data.csv'
-COUNTRIES = ['United States', 'Norway']
+COUNTRIES = ['United States', 'Norway', 'India', 'South Korea', 'Japan', 'China']
 
