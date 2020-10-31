29ab786412f95507a05d172279cf31065b213487
Agnar22 <agnar.bjornstad@gmail.com>
2020-10-31 17:33:51 +0100

    Add parameters for LSTM.

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-# Data related parameters.
+# Data parameters.
 DATA_PATH = 'Data/owid-covid-data.csv'
 COUNTRIES = ['Norway']
 INPUTDAYS = 14
 VALIDATION_SIZE = 0.2
 INFECTED_LOWER = 100
+
+# LSTM parameters.
+UNITS = 200
+
 
