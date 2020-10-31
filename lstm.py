181364ba046148e3188ffa9a6ca1f5eb1a3403f1
Agnar22 <agnar.bjornstad@gmail.com>
2020-10-31 18:42:58 +0100

    Add arguments to optimizer.

--- lstm.py
+++ lstm.py
@@ -1,5 +1,6 @@
 from keras.layers import Dense, Flatten, LSTM, RepeatVector
 from keras.models import Sequential
+from keras.optimizers import Adam
 import numpy as np
 import config
 
@@ -8,7 +9,8 @@ def create_model():
   model = Sequential()
   model.add(LSTM(config.UNITS, input_shape=(config.INPUTDAYS, 1), return_sequences=False))
   model.add(Dense(1))
-  model.compile(loss='mse', optimizer='adam')
+  optim = Adam(learning_rate=0.0001)
+  model.compile(loss='mse', optimizer=optim)
   model.summary()
   return model
 
