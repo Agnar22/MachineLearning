7bb377d2bd745ed75c5b4ab2dbc827079bec8bc2
Agnar22 <agnar.bjornstad@gmail.com>
2020-11-23 02:23:19 +0100

    Add comment in config.

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ INPUTDAYS = 21
 VALIDATION_SIZE = 0.2
 INFECTED_LOWER = 100
 LSTM_MODEL_DIR = 'Models'
+
+# ConfirmedCases has to be the last column as this is used when indexing.
 FEATURES = ['C1_School closing', 'C2_Workplace closing', 'C3_Cancel public events', 'C4_Restrictions on gatherings',
             'C5_Close public transport', 'C6_Stay at home requirements', 'C7_Restrictions on internal movement',
             'C8_International travel controls', 'H1_Public information campaigns', 'H2_Testing policy',
