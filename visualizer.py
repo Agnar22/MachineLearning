918e143aa6dcc92163d4a84a40ff7e3d89873385
Agnar22 <agnar.bjornstad@gmail.com>
2020-11-26 23:26:14 +0100

    Display government responses.

--- visualizer.py
+++ visualizer.py
@@ -1,10 +1,11 @@
 from main import load_and_clean_data
 import matplotlib.pyplot as plt
+import config
 
 def normalize(data):
   return data/max(data)
 
-def draw_graph_stacked(*args, x: str = 'x', y: str = 'y'):
+def draw_graph_stacked(*args):
   """
   :param args: dict('x' : list, 'y' : list, 'name' : str)
   :param y: label for y axis.
@@ -14,20 +15,17 @@ def draw_graph_stacked(*args, x: str = 'x', y: str = 'y'):
 
   plt.close('all')
   fig, axs = plt.subplots(len(args), sharex=True)
+  plt.subplots_adjust(left=None, bottom=None, right=None, top=0.2, wspace=None, hspace=1.0)
   counter = 0
   for func in args:
     axs[counter].plot(func['x'], func['y'])
     axs[counter].set_title(func['name'], fontsize=10)
     counter += 1
-  fig.suptitle('Health system policies')
 
-
-if __name__ == '__main__':
+def display_policies(health:bool=False):
   cutoff = -1
   data = load_and_clean_data()
-  features = ['H1_Public information campaigns', 'H2_Testing policy',
-            'H3_Contact tracing', 'H4_Emergency investment in healthcare', 'H5_Investment in vaccines',
-            'H6_Facial Coverings', 'ConfirmedCases']
+  features = list(filter(lambda x: (health and 'H' == x[0]) or (not health and 'C' == x[0]), config.FEATURES))
   test_x = data[data['CountryName'] == 'Norway']['Date'].values[:cutoff]
   test_dict = []
   for feature in features[:-1]:
@@ -35,4 +33,7 @@ if __name__ == '__main__':
     test_dict.append({'x':test_x,'y':test_feature,'name':feature})
   draw_graph_stacked(*test_dict)
   plt.show()
-  
+
+if __name__ == '__main__':
+  display_policies(True)
+  display_policies(False)
